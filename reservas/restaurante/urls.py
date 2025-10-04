c67e6ff8674d5fd21b0b681f42dc4446529dd6ce
Goolle <juanguillermosanchezayala@gmail.com>
2025-10-03 21:23:22 -0300

    Delete reservas directory

--- reservas/restaurante/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.lista_reservas, name='lista_reservas'),
-    path('nueva/', views.nueva_reserva, name='nueva_reserva'),
-    path('cancelar/<int:id>/', views.cancelar_reserva, name='cancelar_reserva')
-]
